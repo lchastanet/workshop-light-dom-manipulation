f502e874107b5a727aa357038c89f47d3b54d930
Loris <45939615+lchastanet@users.noreply.github.com>
2022-09-07 08:58:52 +0200

    add correction

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
+const btn = document.querySelector("#switcher")
+let globalTheme = "light"
 
+const getCSSValueOf = (htmlNode, property) =>
+  getComputedStyle(htmlNode).getPropertyValue(property)
+
+const switchOnOff = (e) => {
+  const [color, name] =
+    globalTheme === "dark" ? ["light", "Nuit"] : ["dark", "Jour"]
+
+  const body = document.querySelector("body")
+
+  body.style.backgroundColor = getCSSValueOf(body, `--${color}`)
+
+  e.target.textContent = name
+
+  globalTheme = color
+
+  const clouds = document.querySelectorAll(".cloud")
+
+  clouds.forEach((cloud) => {
+    cloud.style.setProperty(
+      `--lightCloud`,
+      getCSSValueOf(body, `--${color}Cloud`)
+    )
+  })
+}
+
+btn.addEventListener("click", switchOnOff)
